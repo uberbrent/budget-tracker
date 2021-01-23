cfa688d2b64485e672f555eda9d53fcb530d8c3a
uberbrent <bmjohns1337@gmail.com>
2021-01-23 16:40:03 -0600

    added near full functionality

--- public/js/idb.js
+++ public/js/idb.js
@@ -24,11 +24,19 @@ function saveRecord(record) {
 
     const budgetObjectStore = transaction.objectStore('new_budget');
 
+    budgetObjectStore.add(record);
+};
+
+function uploadData() {
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+
+    const budgetObjectStore = transaction.objectStore('new_budget');
+
     const getAll = budgetObjectStore.getAll();
 
     getAll.onsuccess = function() {
         if(getAll.result.length > 0) {
-            fetch('/api/budgets', {
+            fetch('/api/transaction', {
                 method: 'POST',
                 body: JSON.stringify(getAll.result),
                 headers: {
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,26 @@
 const FILES_TO_CACHE = [
+    "/",
+    "./manifest.json",
     "./index.html",
     "./css/styles.css",
     "./js/index.js",
-    "./js/idb.js"
+    "./js/idb.js",
+    "./icons/icon-72x72.png",
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-152x152.png",
+    "./icons/icon-192x192.png",
+    "./icons/icon-384x384.png",
+    "./icons/icon-512x512.png",
+    "../routes/api.js"
 ];
 
 const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
-self.addEventListener('install', function(evt) {
+self.addEventListener('install', function (evt) {
     evt.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
             console.log('Your files were pre-cached successfully!');
@@ -19,12 +30,12 @@ self.addEventListener('install', function(evt) {
     self.skipWaiting();
 });
 
-self.addEventListener('activate', function(evt) {
+self.addEventListener('activate', function (evt) {
     evt.waitUntil(
         caches.keys().then(keyList => {
             return Promise.all(
                 keyList.map(key => {
-                    if(key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                    if (key !== CACHE_NAME) {
                         console.log('Removing old cache data', key);
                         return caches.delete(key);
                     }
@@ -32,5 +43,21 @@ self.addEventListener('activate', function(evt) {
             );
         })
     );
+
     self.clients.claim();
+});
+
+self.addEventListener('fetch', function (evt) {
+    console.log('fetch request : ' + evt.request.url)
+    evt.respondWith(
+        caches.match(evt.request).then(function (request) {
+            if(request) {
+                console.log('responding with cache : ' + evt.request.url)
+                return request
+            } else {
+                console.log('file is not cached, fetching : ' + evt.request.url)
+                return fetch(evt.request)
+            }
+        })
+    )
 })
